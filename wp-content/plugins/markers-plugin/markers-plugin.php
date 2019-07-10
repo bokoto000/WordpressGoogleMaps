7ec52bf11c371f868754c352f74c6383385429d3
bokoto000 <bobi_velk@abv.bg>
2019-07-10 23:38:03 +0300

    Plugin update

--- wp-content/plugins/markers-plugin/markers-plugin.php
+++ wp-content/plugins/markers-plugin/markers-plugin.php
@@ -7,14 +7,24 @@
 * Author: Boris V
 * Author URI: http://borisvelkovski.com/
 **/
+
+
+if( ! defined('ABSPATH')){
+    die('Not ok');
+}
+
+
+
 function dh_get_markers(){
+    echo `
+    `;
     global $wpdb;
-    $results = $wpdb->get_results( "SELECT * FROM $wpdb->googlemaps_locations ");
-    foreach($results['data'] as $result) {
-        echo $result['type'], '<br>';
-    }
-    return $results;
+    $row = $wpdb->get_results( "SELECT * FROM googlemaps_locations");
+    foreach ( $row as $row ) 
+    { echo "Location Id:".$row->id."Location Data1:".$row->len."Location Data2:".$row->lng."|||||";} //$row->your_column_name in table
+    $javascript_ouput = "<script>console.log('Debug Info:');</script>";
+    echo $javascript_ouput;
 }
 add_action( 'pre_get_posts', 'dh_get_markers');
-dh_get_markers();
+
 ?>
